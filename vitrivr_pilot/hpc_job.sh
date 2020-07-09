de11aec5d6b722cd726338780a7b73d6cbbe1554
Frankie Robertson <frankie@robertson.name>
2020-07-09 11:58:24 +0300

    Fix up cineast indexing on SLURM cluster
    * Use lowercase extractor names like cineast seems to want
    * Run cineast setup before extraction
    * Specify image cache directory
    * Fix up ssh tunnel command

--- vitrivr_pilot/hpc_job.sh
+++ vitrivr_pilot/hpc_job.sh
@@ -10,12 +10,23 @@ exec ssh -A $SLURM_JOB_NODELIST bash <<-SSHSCOPE
 	module load singularity
 
 	cd $(pwd)
-	ssh -L 127.0.0.1:1865:127.0.0.1:1865 gallo.cosi.cwru.edu &
+
+	# Set up tunnel to cottontail
+	ssh -N -L 127.0.0.1:1865:127.0.0.1:1865 gallo.cosi.cwru.edu &
+
+	# DB setup
+	singularity run \
+	  cineast.sif $(pwd)/cineast.json \
+	  setup
+
+	# Extraction
 	mkdir -p cineast/logs
 	mkdir -p cineast/out
+	mkdir -p cineast/cache
 	singularity run \
 	  --bind cineast/logs:/opt/cineast/logs \
 	  --bind cineast/out:/opt/cineast/out \
+	  --bind cineast/cache:/opt/cineast/cache \
           $EXTRA_SINGULARITY_ARGS \
 	  cineast.sif $(pwd)/cineast.json \
 	  extract --extraction $(pwd)/cineast_job.json && touch $1
